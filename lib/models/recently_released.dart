d0ade551e2199a1faaf1b1c8a06effe5a45b31fe
Anmol Jhamb <talktoanmol@outlook.com>
2023-05-02 22:17:53 +0530

    model added for recently_released

---
+++ lib/models/recently_released.dart
@@ -0,0 +1,27 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part "recently_released.g.dart";
+
+@JsonSerializable()
+class RecentlyReleased {
+  final String episodeId;
+  final String animeTitle;
+  final String episodeNum;
+  final String subOrDub;
+  final String animeImg;
+  final String episodeUrl;
+
+  RecentlyReleased({
+    required this.episodeId,
+    required this.animeTitle,
+    required this.episodeNum,
+    required this.subOrDub,
+    required this.animeImg,
+    required this.episodeUrl,
+  });
+
+  factory RecentlyReleased.fromJson(Map<String, dynamic> json) =>
+      _$RecentlyReleasedFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RecentlyReleasedToJson(this);
+}
---
+++ lib/models/recently_released.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recently_released.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RecentlyReleased _$RecentlyReleasedFromJson(Map<String, dynamic> json) =>
+    RecentlyReleased(
+      episodeId: json['episodeId'] as String,
+      animeTitle: json['animeTitle'] as String,
+      episodeNum: json['episodeNum'] as String,
+      subOrDub: json['subOrDub'] as String,
+      animeImg: json['animeImg'] as String,
+      episodeUrl: json['episodeUrl'] as String,
+    );
+
+Map<String, dynamic> _$RecentlyReleasedToJson(RecentlyReleased instance) =>
+    <String, dynamic>{
+      'episodeId': instance.episodeId,
+      'animeTitle': instance.animeTitle,
+      'episodeNum': instance.episodeNum,
+      'subOrDub': instance.subOrDub,
+      'animeImg': instance.animeImg,
+      'episodeUrl': instance.episodeUrl,
+    };
