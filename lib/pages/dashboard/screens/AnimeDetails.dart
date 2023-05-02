9a898888d4523e745972891f7909a174cfffac63
Anmol Jhamb <talktoanmol@outlook.com>
2023-05-03 00:18:25 +0530

    anime details screen added

---
+++ lib/pages/dashboard/screens/AnimeDetails.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimeDetailsScreen extends StatelessWidget {
+  final String animeTitle;
+
+  const AnimeDetailsScreen({
+    super.key,
+    required this.animeTitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
