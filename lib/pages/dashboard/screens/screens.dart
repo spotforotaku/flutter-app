015776f12515fe92a694edd792885229f5e0f3f4
Anmol Jhamb <talktoanmol@outlook.com>
2023-05-02 13:59:28 +0530

    moved profile_screen in another folder inside screens

--- lib/pages/dashboard/screens/screens.dart
+++ lib/pages/dashboard/screens/screens.dart
@@ -1,5 +1,5 @@
 export 'discussion_screen.dart';
 export 'home_screen.dart';
 export 'list_screen.dart';
-export 'profile_screen.dart';
+export 'profile/profile_screen.dart';
 export 'recommend_screen.dart';
