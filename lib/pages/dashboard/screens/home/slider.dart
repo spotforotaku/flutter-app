504e9020a8d146026b9cede0f5f94b9e4902243c
Anmol Jhamb <talktoanmol@outlook.com>
2023-05-03 00:38:04 +0530

    anime slider wrapped with inkwell

--- lib/pages/dashboard/screens/home/slider.dart
+++ lib/pages/dashboard/screens/home/slider.dart
@@ -20,33 +20,38 @@ class AnimeSlider extends StatelessWidget {
           enlargeCenterPage: true,
           itemCount: animes.length,
           itemBuilder: (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(
-                  21,
-                ),
-              ),
-              child: Stack(
-                children: [
-                  Image(
-                    image: NetworkImage(
-                      animes[index].animeImg,
-                    ),
-                    height: 300,
-                    fit: BoxFit.contain,
+            return InkWell(
+              onTap: () {
+                print("tappen on index $index");
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    21,
                   ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.favorite_outline_rounded,
-                        color: Colors.white,
+                ),
+                child: Stack(
+                  children: [
+                    Image(
+                      image: NetworkImage(
+                        animes[index].animeImg,
+                      ),
+                      height: 300,
+                      fit: BoxFit.contain,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.favorite_outline_rounded,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           },
